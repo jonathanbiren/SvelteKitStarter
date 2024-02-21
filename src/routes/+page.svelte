fa35c9c19f83697f925e5203624ffb6753dd9320
Jonathan Biren <jonathan.biren@unibw.de>
2024-02-21 07:44:45 +0100

    Initial commit. You can use This commit as a starter for other SvelteKit projects

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="ml-3">
+	<button class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+		Test Tailwind CSS
+	</button>
+	<br />
+	<button class="btn btn-primary mt-3">This is a DaisyUI button</button>
+</div>
